c862c0e9e4f6c09d28f7820d20744763edf47fdb
Krunal Sheth <krunal.sheth@verizon.com>
2016-05-07 18:50:09 -0400

    CLient server commn

--- client/main/main.controller.ng.js
+++ client/main/main.controller.ng.js
@@ -1,83 +1,42 @@
 'use strict'
 
 angular.module('techApp')
-.controller('MainCtrl', function($scope) {
-  $("#loginModal").modal({backdrop: false});
+.controller('MainCtrl', function($scope,  $meteor) {
 
-  $scope.submitLoginForm = function(currentUser){
-        // 1. Attempt to login.
-        Meteor.loginWithPassword(currentUser.userName, currentUser.userPassword, function(error) {
-          // 2. Handle the response
-          if(Meteor.user()) {
-            // Redirect the user to where they're loggin into. Here, Router.go uses
-            $("#loginModal").modal('hide');
-                    $location.path(Session.get('toState'));
-          } else {
-             // If no user resulted from the attempt, an error variable will be available
-                  // in this callback. We can output the error to the user here.
-                  $scope.showErrorMessage = true;
+
+  //Subscribe to the DB you wana use
+  $scope.$meteorSubscribe('staff');
+
+  $scope.submitLoginForm = function(){
+
+      /*
+      * Create a Post in the List table
+      */
+      $meteor.call('getAccessToken',$scope.userObj).then(
+          function(data) {
+            if(angular.isDefined(data)) {
+              debugger;
+            }    
+          },
+         function(err) {
+            alert(err.error);
           }
-        });
+        ); 
+
+        // // 1. Attempt to login.
+        // Meteor.loginWithPassword(currentUser.userName, currentUser.userPassword, function(error) {
+        //   // 2. Handle the response
+        //   if(Meteor.user()) {
+        //     // Redirect the user to where they're loggin into. Here, Router.go uses
+        //     $("#loginModal").modal('hide');
+        //             $location.path(Session.get('toState'));
+        //   } else {
+        //      // If no user resulted from the attempt, an error variable will be available
+        //           // in this callback. We can output the error to the user here.
+        //           $scope.showErrorMessage = true;
+        //   }
+        // });
       }
 
-      $scope.showRegistrationForm = function(newUser){
-        $("#loginModal").modal('hide');
-        $("#registerModal").modal({backdrop: false});
-      }
 
-      $scope.submitRegistrationForm = function(newUser) {
-        Accounts.createUser({
-          email: newUser.userName,
-          password: newUser.userPassword,
-          profile: 'user'
-        });
-        $("#registerModal").modal('hide');
-            $location.path(Session.get('toState'));
-      }
-  $scope.page = 1;
-  $scope.perPage = 3;
-  $scope.sort = {name_sort : 1};
-  $scope.orderProperty = '1';
-  
-  $scope.helpers({
-    things: function() {
-      return Things.find({}, {
-        sort: $scope.getReactively('sort') 
-      });
-    },
-    thingsCount: function() {
-      return Counts.get('numberOfThings');
-    }
-  });
-                  
-  $scope.subscribe('things', function() {
-    return [{
-      sort: $scope.getReactively('sort'),
-      limit: parseInt($scope.getReactively('perPage')),
-      skip: ((parseInt($scope.getReactively('page'))) - 1) * (parseInt($scope.getReactively('perPage')))
-    }, $scope.getReactively('search')];
-  });
-
-  $scope.save = function() {
-    if ($scope.form.$valid) {
-      Things.insert($scope.newThing);
-      $scope.newThing = undefined;
-    }
-  };
-                  
-  $scope.remove = function(thing) {
-    Things.remove({_id: thing._id});
-  };
-                  
-  $scope.pageChanged = function(newPage) {
-    $scope.page = newPage;
-  };
-                  
-  return $scope.$watch('orderProperty', function() {
-    if ($scope.orderProperty) {
-      $scope.sort = {
-        name_sort: parseInt($scope.orderProperty)
-      };
-    }
-  });
 });
--- server/staff.publish.js
+++ server/staff.publish.js
@@ -10,11 +10,12 @@ Meteor.publish('staff', function (options, searchString) {
     return Staff.find(where, options);
 });
 Meteor.methods({
-        getAccessToken: function (username, password) {
+        getAccessToken: function (userObj) {
+            
             var access_token_value;
             var role_value;
-            HTTP.call('GET',
-                'http://sandbox.graph.hmhco.com/v4/sample_token?client_id=a6d66350-9c25-45cc-92c7-d674ad9496f1.hmhco.com&grant_type=password&username=' + username + '&password=' + password,
+            HTTP.call('POST',
+                'http://sandbox.graph.hmhco.com/v4/sample_token?client_id=a6d66350-9c25-45cc-92c7-d674ad9496f1.hmhco.com&grant_type=password&username=' + userObj.username + '&password=' + userObj.password,
                 {
                     headers: {
                         "Accept": "application/json",
@@ -23,32 +24,33 @@ Meteor.methods({
                     }
                 },
                 function (error, response) {
-                    console.log(response.data);
+                    console.log(response.data.roles);
                     access_token_value = response.data.access_token;
-                    console.log(access_token_value);
+                    //console.log(access_token_value);
+                     return response.data.roles;
                 }
             );
-            HTTP.call('GET',
-                "http://sandbox.graph.hmhco.com/v4/me",
-                {
-                    headers: {
-                        "Accept": "application/json",
-                        "Authorization": access_token_value,
-                        "Vnd-HMH-Api-Key": "558063932b65c556ebd5676e45a0b8cd"
-                    }
-                },
-                function (error, response) {
-                    console.log(response.data);
-                    role_value = response.data.role;
-                    console.log(role_value);
-                }
-            );
-            var res =
-            {
-                'access_token': access_token_value,
-                'role': role_value
-            };
-            return res;
+            // HTTP.call('GET',
+            //     "http://sandbox.graph.hmhco.com/v4/me",
+            //     {
+            //         headers: {
+            //             "Accept": "application/json",
+            //             "Authorization": access_token_value,
+            //             "Vnd-HMH-Api-Key": "558063932b65c556ebd5676e45a0b8cd"
+            //         }
+            //     },
+            //     function (error, response) {
+            //         console.log(response.data);
+            //         role_value = response.data.role;
+            //         console.log(role_value);
+            //     }
+            // );
+            // var res =
+            // {
+            //     'access_token': access_token_value,
+            //     'role': role_value
+            // };
+           
         },
         getLectures: function () {
             return "";
