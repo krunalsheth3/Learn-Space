d0194a74f01c1f723edd70bbe63873acefc77e26
Sean Wu <sean.wu@verizon.com>
2016-05-07 22:51:09 -0400

    finish apis for record

--- server/hmhuser.publish.js
+++ server/hmhuser.publish.js
@@ -12,7 +12,7 @@ Meteor.publish('hmhuser', function (options, searchString) {
 Meteor.methods({
         getAccessToken: function (userObj) {
             console.log(userObj.username);
-            var getLecturesResponse = {};
+            console.log(userObj.password);
             // v4/sample_token
             HTTP.call('POST',
                 'http://sandbox.graph.hmhco.com/v4/sample_token?client_id=a6d66350-9c25-45cc-92c7-d674ad9496f1.hmhco.com&grant_type=password&username=' + userObj.username + '&password=' + userObj.password,
@@ -40,14 +40,44 @@ Meteor.methods({
                         Hmhuser.update({"preferred_username": getAccessTokenResponse.preferred_username}, {$set: getAccessTokenResponse});
                         console.log("updating user: " + getAccessTokenResponse.preferred_username);
                     }
-                    //console.log(getAccessTokenResponse);
-                    // v4/rosters
+                    return getAccessTokenResponse;
+                }
+            );
+        },
+        getLectures: function (authObj) {
+            console.log(authObj.access_token);
+            console.log(authObj.roles);
+            if(authObj.roles.toUpperCase() === 'learner'.toUpperCase()){
+                return Lecture.find();
+            }
+            // v4/rosters
+            HTTP.call('GET',
+                "http://sandbox.graph.hmhco.com/v4/rosters?facet=detail&include=staff%2Cstudents",
+                {
+                    headers: {
+                        "Accept": "application/json",
+                        "Authorization": authObj.access_token,
+                        "Vnd-HMH-Api-Key": "558063932b65c556ebd5676e45a0b8cd"
+                    }
+                },
+                function (error, response) {
+                    if (error != null) {
+                        var errorRes = {
+                            'errorCode': '-1',
+                            'errorMessage': error.message
+                        };
+                        return errorRes;
+                    }
+                    var getLecturesResponse = response.data;
+                    //console.log(getLecturesResponse);
+                    // v4/documents
                     HTTP.call('GET',
-                        "http://sandbox.graph.hmhco.com/v4/rosters?facet=detail&include=staff%2Cstudents",
+                        "http://sandbox.graph.hmhco.com/v4/documents",
                         {
                             headers: {
                                 "Accept": "application/json",
-                                "Authorization": getAccessTokenResponse.access_token,
+                                "Authorization": authObj.access_token,
+                                "Content-Type": "application/json",
                                 "Vnd-HMH-Api-Key": "558063932b65c556ebd5676e45a0b8cd"
                             }
                         },
@@ -59,43 +89,56 @@ Meteor.methods({
                                 };
                                 return errorRes;
                             }
-                            getLecturesResponse = response.data;
-                            //console.log(getLecturesResponse);
-                            // v4/documents
-                            HTTP.call('GET',
-                                "http://sandbox.graph.hmhco.com/v4/documents",
-                                {
-                                    headers: {
-                                        "Accept": "application/json",
-                                        "Authorization": getAccessTokenResponse.access_token,
-                                        "Content-Type": "application/json",
-                                        "Vnd-HMH-Api-Key": "558063932b65c556ebd5676e45a0b8cd"
-                                    }
-                                },
-                                function (error, response) {
-                                    var getDocumentsResponse = response.data;
-                                    //console.log(getDocumentsResponse);
-                                    for (var i = 0; i < getLecturesResponse.data.length; i++) {
-                                        getLecturesResponse.data[i]["videos"] = getDocumentsResponse.data;
-                                    }
-                                    //console.log(getLecturesResponse);
-                                    //console.log(getLecturesResponse.data[0]);
-                                    for (var i = 0; i < getLecturesResponse.data.length; i++) {
-                                        if (Lecture.find({"course_id": getLecturesResponse.data[i].course_id}).count() == 0) {
-                                            Lecture.insert(getLecturesResponse.data[i]);
-                                            console.log("inserting lecture: " + getLecturesResponse.data[i].course_id);
-                                        } else {
-                                            Lecture.update({"course_id": getLecturesResponse.data[i].course_id}, {$set: getLecturesResponse.data[i]});
-                                            console.log("updating lecture: " + getLecturesResponse.data[i].course_id);
-                                        }
-                                    }
-                                    return getLecturesResponse;
+                            var getDocumentsResponse = response.data;
+                            for (var i = 0; i < getLecturesResponse.data.length; i++) {
+                                getLecturesResponse.data[i]["videos"] = getDocumentsResponse.data;
+                            }
+                            for (var i = 0; i < getLecturesResponse.data.length; i++) {
+                                if (Lecture.find({"course_id": getLecturesResponse.data[i].course_id}).count() == 0) {
+                                    Lecture.insert(getLecturesResponse.data[i]);
+                                    console.log("inserting lecture: " + getLecturesResponse.data[i].course_id);
+                                } else {
+                                    Lecture.update({"course_id": getLecturesResponse.data[i].course_id}, {$set: getLecturesResponse.data[i]});
+                                    console.log("updating lecture: " + getLecturesResponse.data[i].course_id);
                                 }
-                            );
+                            }
+                            return getLecturesResponse;
                         }
                     );
                 }
             );
+        },
+        postRecord: function (recordObj) {
+            console.log(recordObj.access_token);
+            console.log(recordObj.roles);
+            console.log(recordObj.secure_token);
+            console.log(recordObj.tag);
+            console.log(recordObj.time);
+            if (recordObj.roles.toUpperCase() === 'instructor'.toUpperCase()) {
+                var errorRes = {
+                    'errorCode': '-1',
+                    'errorMessage': "role unsupported for this operation"
+                };
+                return errorRes;
+            }
+
+            var hmhuser = Hmhuser.find({"access_token":recordObj.access_token});
+            recordObj["user"]=hmhuser;
+
+            Record.insert(recordObj);
+        },
+        getRecords: function (videoObj) {
+            console.log(videoObj.access_token);
+            console.log(videoObj.roles);
+            console.log(videoObj.secure_token);
+            if (recordObj.roles.toUpperCase() === 'learner'.toUpperCase()) {
+                var errorRes = {
+                    'errorCode': '-1',
+                    'errorMessage': "role unsupported for this operation"
+                };
+                return errorRes;
+            }
+            return Record.find({"secure_token":videoObj.secure_token});
         }
     }
 )
